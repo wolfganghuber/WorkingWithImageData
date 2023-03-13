492238239e62997cebd8d60622fb07ca36d13c6a
Wolfgang Huber <wolfgang.huber@embl.org>
2023-03-13 20:59:39 +0100

    File management

---
+++ resources/rsync.sh
@@ -0,0 +1,2 @@
+% Backup of the large (movie) files to EMBL server:
+rsync -avz /Users/whuber/svnco/WorkingWithImageData/resources/ whuber@datatransfer.embl.de:/g/huber/www-huber/users/whuber/2304-Imaging-Data-in-R/resources
