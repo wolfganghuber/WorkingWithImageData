e876ae330004826d3d2811ebed1706c1424bc9ed
Wolfgang Huber <wolfgang.huber@embl.org>
2023-03-18 22:36:25 +0100

    CLeaned up the dependency management

--- resources/rsync.sh
+++
@@ -1,2 +0,0 @@
-# Backup of the large (movie) files to EMBL server:
-rsync -avz /Users/whuber/svnco/WorkingWithImageData/resources/ whuber@datatransfer.embl.de:/g/huber/www-huber/users/whuber/2304-Imaging-Data-in-R/resources
